29dfc92e83ed6863d002755113106e8682258452
srivatsa <you@example.com>
2019-08-07 00:30:26 -0700

    appspec modified

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 
-sleep 3600 &
-pid=$!
-sleep 5
+fuser -k 8080/tcp
 
-echo ===
-echo PID is $pid, before kill:
-ps -ef | grep -E "PPID|$pid" | sed 's/^/   /'
-echo ===
-
-( kill -TERM $pid ) 2>&1
-sleep 5
-
-echo ===
-echo PID is $pid, after kill:
-ps -ef | grep -E "PPID|$pid" | sed 's/^/   /'
-echo ===
